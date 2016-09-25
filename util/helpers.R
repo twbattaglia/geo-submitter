6e21db1f2601a8affc445beedd39fcde5327e5b5
Thomas Battaglia <tb1280@nyu.edu>
2016-09-25 01:43:11 -0400

    clean up and added introduction
    cleaned up to look more ‘professional’
    switched to camelCase
    changed variables for the eventual addition of other platforms
    remove unused function
    added prilimiary support for microarray data (still work to be done)

--- util/helpers.R
+++ util/helpers.R
@@ -1,35 +1,23 @@
 library(shinyjs)
 library(knitr)
 library(dplyr)
-
+library(highcharter)
+library(NanoStringNorm)
+library(ggplot2)
+library(reshape2)
+library(rhandsontable)
 library(affy)
 library(gcrma)
 
 # Larger Text Area Input
-textareaInput <- function(inputId, label, value="", placeholder="", rows=5){
+textareaInput <- function(inputId, label, value = "", placeholder = "", rows = 4){
   tagList(
-    div(strong(label), style="margin-top: 5px;"),
-    tags$style(type="text/css", "textarea {width:100%; margin-top: 5px;}"),
+    div(strong(label), style = "margin-top: 5px;"),
+    tags$style(type = "text/css", "textarea {width:100%; margin-top: 5px;}"),
     tags$textarea(id = inputId, placeholder = placeholder, rows = rows, value))
 }
 
 # For writing data to file
 write_append <- function(..., filename) {
   write(x = ..., file = filename, append = TRUE)
-}
-
-
-# Fix upload file names
-# from https://github.com/daattali/advanced-shiny/blob/master/upload-file-names/app.R
-fixUploadedFilesNames <- function(x) {
-  if (is.null(x)) {
-    return()
-  }
-  
-  oldNames = x$datapath
-  newNames = file.path(dirname(x$datapath),
-                       x$name)
-  file.rename(from = oldNames, to = newNames)
-  x$datapath <- newNames
-  x
 }

a5c6e2ba759befa397fbd306c518712c190d8a55
Thomas Battaglia <tb1280@nyu.edu>
2016-09-26 02:53:49 -0400

    fixed writing content bug
    minor formating
    added optional protocols
    added more placeholder text
    switch to shiny textAreaInput function
    added placeholder image for GEO submission

--- util/helpers.R
+++ util/helpers.R
@@ -9,14 +9,6 @@ library(rhandsontable)
 library(affy)
 library(gcrma)
 
-# Larger Text Area Input
-textareaInput <- function(inputId, label, value = "", placeholder = "", rows = 5){
-  tagList(
-    div(label, style = "margin-top: 5px;"),
-    tags$style(type = "text/css", "textarea {width:100%; margin-top: 5px;}"),
-    tags$textarea(id = inputId, placeholder = placeholder, rows = rows, value))
-}
-
 # For writing data to file
 write_append <- function(..., filename) {
   write(x = ..., file = filename, append = TRUE)
